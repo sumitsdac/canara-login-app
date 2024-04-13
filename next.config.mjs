69e9dbc35c9558cfc5a3b90ae5295b5483e08549
sumit_s25 <sumit.s@digitalapicraft.com>
2024-04-13 23:26:18 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ const nextConfig = {
   sassOptions: {
     includePaths: [Path.join(__dirname, "app/styles")],
   },
-  output: "export",
-  distDir: "canara-build",
+  // output: "export",
+  // distDir: "canara-build",
 };
 
 export default nextConfig;
