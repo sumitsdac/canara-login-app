aa83606f0a6aed8eb0fcf43ba4e55fabb924144d
sumit_s25 <sumit.s@digitalapicraft.com>
2024-04-13 23:18:04 +0530

    jndks

--- src/app/userauth/login/verify/route.js
+++ src/app/userauth/login/verify/route.js
@@ -1,28 +1,32 @@
 import { VERIFY_LOGIN_API } from "../../../_lib/api";
 
 export async function POST(req) {
-  const { username, password, session_identifier } = await req.json();
+  try {
+    const { username, password, session_identifier } = await req.json();
 
-  const headersObj = {
-    "Content-type": "application/json",
-  };
+    const headersObj = {
+      "Content-type": "application/json",
+    };
 
-  const bodyContent = JSON.stringify({
-    username,
-    password,
-    session_identifier,
-  });
-  return await new Promise((resolve, reject) => {
-    fetch(VERIFY_LOGIN_API, {
-      method: "post",
-      headers: headersObj,
-      body: bodyContent,
-    })
-      .then((response) => {
-        resolve(response);
+    const bodyContent = JSON.stringify({
+      username,
+      password,
+      session_identifier,
+    });
+    return await new Promise((resolve, reject) => {
+      fetch(VERIFY_LOGIN_API, {
+        method: "post",
+        headers: headersObj,
+        body: bodyContent,
       })
-      .catch((e) => {
-        reject(e);
-      });
-  });
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  } catch (err) {
+    new Response(err);
+  }
 }
